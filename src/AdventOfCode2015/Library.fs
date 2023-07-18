0d6b7b4ea2766a2f61609a71db0fab6e2e2516bd
Nico Loos <nico.loos@slicr.de>
2023-07-18 17:11:28 +0200

    Add Solution Structure

---
+++ src/AdventOfCode2015/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace AdventOfCode2015
+
+module Say =
+  let hello name =
+    printfn "Hello %s" name
---
+++ test/AdventOfCode2015.Tests/UnitTest1.fs
@@ -0,0 +1,11 @@
+module AdventOfCode2015.Tests
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
