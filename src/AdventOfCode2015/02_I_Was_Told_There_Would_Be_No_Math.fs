a230891228da4f474d43c2bd1f5705543d22dbbb
Nico Loos <nico.loos@slicr.de>
2023-07-19 20:50:44 +0200

    02: I was told there would be no math

---
+++ src/AdventOfCode2015/02_I_Was_Told_There_Would_Be_No_Math.fs
@@ -0,0 +1,33 @@
+namespace AdventOfCode2015
+
+module IWasToldThereWouldBeNoMath =
+  let private scanLine (line: string) =
+    line.Split("x")
+    |> Seq.map int
+    |> Seq.toList
+  
+  let calculateWrappingPaperAmount (lines: string) =
+    let parseLine (line: string) =
+      let arr = scanLine line
+      
+      let sides = [| arr[0] * arr[1]; arr[0] * arr[2]; arr[1] * arr[2] |]
+      let area = 2 * Seq.sum sides
+      area + Seq.min sides
+      
+    
+    lines.Split('\n')
+    |> Seq.map parseLine
+    |> Seq.reduce (+)
+    
+  let calculateRibbonLength (lines: string) =
+    let parseLine (line: string) =
+      let arr = scanLine line
+      
+      let volume = Seq.reduce (*) arr
+      let sideLength = (Seq.sum arr - Seq.max arr) * 2
+      
+      volume + sideLength
+
+    lines.Split('\n')
+    |> Seq.map parseLine
+    |> Seq.reduce (+)
